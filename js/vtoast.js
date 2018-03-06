e8e1fdeb2901cb05b1a8086dd18da618721936d4
StevenT96 <steveng.carrion@gmail.com>
2018-03-06 20:13:16 +0100

    Añadido VToast para mostrar mensaje de información o error al usuario

---
+++ js/vtoast.js
@@ -0,0 +1,25 @@
+var VToast = {
+
+    mostrarMensaje: function (mensaje) {
+
+        console.log(mensaje);
+    },
+    mostrarError: function (mensaje) {
+
+        console.log(mensaje);
+    },
+    mostrarAdvertencia: function (mensaje) {
+        console.log(mensaje);
+    },
+    log: function (mensaje) {
+        console.log(mensaje);
+    },
+    logS: function (mensaje) {
+        console.log("<- " + mensaje + " ->");
+    },
+    logF: function (mensaje) {
+        console.log("<- " + mensaje + " -/>");
+    }
+};
+
+
