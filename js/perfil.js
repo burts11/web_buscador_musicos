112c36e3eb33b928cd0636c022c2548a7b3ec690
StevenT96 <steveng.carrion@gmail.com>
2018-03-19 16:18:15 +0100

    Ahora el layout cambia dependiendo del usuario logueado para modificar las propiedades de cada usuario

--- js/perfil.js
+++ js/perfil.js
@@ -1,10 +1,44 @@
 onJqueryWindowCallbackEventOne(VInfo.PERFIL_INFO, {
 
     callback: function (e) {
+        switch (e.json["TipoUsuario"]) {
 
-        $("#perfil_info_logo").prop("src", e.json.logo).fadeIn();
-        $(".perfil_info_detalles").fadeIn();
+            case "Fan":
+                cargarFan(e);
+                break;
+            case "Musico":
+                cargarMusico(e);
+                break;
+            case "Local":
+                break;
+        }
 
         e.json.vparams.onDialogContentLoaded();
     }
 });
+
+function cargarMusico(e) {
+    loadPage("perfil_musico", {
+
+        success: function (result) {
+            var rootPerfil = $('<div class="__root_perfil">').html(result);
+            $("#perfil_info_container").append(rootPerfil);
+        },
+        error: function (result) {
+            console.log(result);
+        }
+    });
+}
+
+function cargarFan(e) {
+    loadPage("perfil_fan", {
+
+        success: function (result) {
+            var rootPerfil = $('<div class="__root_perfil">').html(result);
+            $("#perfil_info_container").append(rootPerfil);
+        },
+        error: function (result) {
+            console.log(result);
+        }
+    });
+}
