77d389a326ba72ac45b68097a748980c001e8631
StevenT96 <steveng.carrion@gmail.com>
2018-03-19 16:20:10 +0100

    Ahora al clickar en un músico se lleva a una página que muestra toda su información y sus canciones

--- js/pagina_principal.js
+++ js/pagina_principal.js
@@ -51,11 +51,17 @@ function cargarPorGeneros() {
 //                VLog.log(json);
                 var musico = $("<div>").addClass("musicoGeneroWrapperContainer");
 
+                $(musico).unbind("click").bind("click", function () {
+
+                    cambiarHash("musico_info_v2");
+                    queueEvent(VInfo.MUSICO_INFO_V2, {data: json});
+                });
+
                 var fullPath = Main.obtenerUserDataPath(json["usuario"]) + "img/album_art.jpg";
                 var img = $('<img>').prop("src", fullPath);
                 $(img).addClass("musicoGeneroWrapperImage clickableElement");
 
-                var musicoGeneroWrapperInfo = $("<div class=' musicoGeneroWrapperInfo centeredElementHorizontal' ></div>");
+                var musicoGeneroWrapperInfo = $("<div class='musicoGeneroWrapperInfo centeredElementHorizontal' ></div>");
 
                 var musicoLabel = $("<label>").addClass("blockLabel whiteText divPadding10");
                 $(musicoLabel).text(json["nombreartistico"]);
@@ -96,8 +102,8 @@ function cargarConciertos()
             $(conciertoBody).click(function () {
 
             });
-            
-            console.log(item);
+
+//            console.log(item);
 
             var fullPath = Main.obtenerUserDataPath(usuario["usuario"]) + "img/concierto.jpg";
             var img = $('<img>').prop("src", fullPath);
@@ -124,6 +130,8 @@ function cargarConciertos()
 function inicializarSlickNovedades() {
 
     $('#divConciertosNovedad').slick({
+        autoplay: true,
+        autoplaySpeed: 1200,
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -143,100 +151,5 @@ function inicializarSlickNovedades() {
                 }
             }
         ]
-    });
-}
-
-function cargarMusicos() {
-    $("#divMusicos").empty();
-
-    var select = "SELECT * FROM usuario INNER JOIN musico ON usuario.idusuario = musico.idmusico;";
-
-    callAjaxBBDD(
-            {
-                action: "RawQuery",
-                query: select
-            }, function (result) {
-
-        $("#divMusicos").empty();
-
-        for (var i = 0; i < 1; i++) {
-
-            $.each(result, function (i, item) {
-
-                var usuario = item;
-//            var nombre = usuario ["nombre"];
-                var nombreartistico = usuario ["nombreartistico"];
-                var genero = usuario ["genero"];
-
-                var div = $("<div>").addClass("musicoContainer");
-
-                var musicoBody = $("<div>").addClass("musicoBody clickableElement");
-                var musicoFooter = $("<div>").addClass("musicoFooter");
-
-                $(musicoBody).append($("<div>").addClass("musicoAlbumArtContainer"));
-                $(musicoBody).append("<div class='musicoBodyOver'>");
-
-                var fullPath = Main.obtenerUserDataPath(usuario["usuario"]) + "img/album_art.jpg";
-                $(musicoBody).find(".musicoAlbumArtContainer").append("<img>").find("img").addClass("musicoAlbumArtImg").prop("src", fullPath);
-
-                $(musicoFooter).append($("<div>").addClass("musicoInfo"));
-
-                var nombreArtisticoDiv = $("<div>").addClass("blockDiv");
-                var nombreArtisticoLang = $("<label lang='es' data-lang-token='MusicoInfoNombreArtistico'></>")
-                        .addClass("musicoInfoTexto").text("Nombre artístico: ");
-                var nombreArtisticoReal = $("<label>").addClass("musicoInfoTexto ").text(nombreartistico);
-
-                $(nombreArtisticoDiv).append(nombreArtisticoLang).append(nombreArtisticoReal);
-
-                var generoDiv = $("<div>").addClass("blockDiv");
-                var generoLang = $("<label lang='es' data-lang-token='MusicoInfoGenero'></>")
-                        .addClass("musicoInfoTexto").text("Género : ");
-                var generoReal = $("<label>").addClass("musicoInfoTexto ").text(genero);
-
-                $(generoDiv).append(generoLang).append(generoReal);
-
-                $(musicoFooter).find(".musicoInfo").append(nombreArtisticoDiv);
-                $(musicoFooter).find(".musicoInfo").append(generoDiv);
-
-                $(div).append(musicoBody);
-                $(div).append(musicoFooter);
-
-                $(musicoBody).bind("click", function () {
-
-                    VModal.show("musico_info", item, {modalEffect: "md-effect-8", VModalId: generateUniqueId()}, {
-                        onDialogShow: function (ev) {
-
-                            var usuarioId = usuario["idusuario"];
-                            ev["usuarioId"] = usuarioId;
-                            ev["vparams"]["sender"] = "musico_info_principal";
-
-                            callJqueryWindowEvent(VInfo.MUSICO_INFO, ev);
-                        },
-                        onDialogClose: function (ev) {
-                        }
-                    });
-                });
-
-                $(musicoBody).mouseenter(function () {
-                    $(musicoBody).find(".musicoBodyOver").stop(true, true).show().addClass("fade-in-musico");
-                    $(musicoBody).find(".musicoAlbumArtImg").addClass("zoomMusicoIn");
-                });
-
-                $(musicoBody).mouseleave(function () {
-                    $(musicoBody).find(".musicoBodyOver").removeClass("fade-in-musico").stop(true, true).fadeOut();
-                    $(musicoBody).find(".musicoAlbumArtImg").removeClass("zoomMusicoIn").addClass("zoomMusicoOut");
-                });
-
-                $("#divMusicos").append(div);
-
-                $(div).addClass("musicoAnimIn");
-
-                setTimeout(function () {
-
-                    $(div).addClass("musicoAnimIn2");
-
-                }, 20);
-            });
-        }
     });
 }
