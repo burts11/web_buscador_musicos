2c65f5109eb1f62297027d4f971d5a34b6e2f212
StevenT96 <steveng.carrion@gmail.com>
2018-03-19 16:16:14 +0100

    Ahora el idioma elegido por el usuario se guarda

--- js/jlang.js
+++ js/jlang.js
@@ -1,30 +1,55 @@
-//appendScript("js/js.cookie.js");
-//appendScript("js/jquery-lang.js");
-
-//onJqueryReady(function () {
-//
-//    load();
-//});
-
 var lang = new Lang();
 lang.dynamic('en', 'langpack/en.json');
 lang.dynamic('es', 'langpack/es.json');
 
 lang.init({
-    defaultLang: 'es',
-    currentLang: 'es'
+    defaultLang: "es",
+    currentLang: "es"
 });
 
-$(".mainButton").click(function () {
-    window.lang.change("en", "", function (e, v, i) {
-        console.log(e);
-        console.log(v);
-        console.log(i);
+var JLang = {
 
-        window.lang.change("en");
+    obtenerIdioma: function () {
+        var idiomaId = localStorage.getItem("idiomaId");
+        if (idiomaId === null) {
+            return "es";
+        }
+        return localStorage.getItem("idiomaId");
+    }
+    ,
+    guardarIdioma: function (idioma) {
+
+        var idiomaId = localStorage.getItem("idiomaId");
+        if (idiomaId === null) {
+            localStorage.setItem("idiomaId", "");
+        }
+
+        localStorage.setItem("idiomaId", idioma);
+    },
+    cambiarIdioma: function (idiomaId) {
+        console.log("Cambiar idioma!" + idiomaId);
+        _cambiarIdioma(idiomaId, function (idiomaId) {
+
+        });
+    },
+    cargarIdiomaDefault: function (func) {
+
+        var id = JLang.obtenerIdioma();
+        _cambiarIdioma(id, function (idiomaId) {
+            func(idiomaId);
+        });
+    }
+};
+
+function _cambiarIdioma(idiomaId, func) {
+
+    window.lang.change(idiomaId, "", function (e, v, i) {
+        window.lang.change(idiomaId);
+        func(idiomaId);
+        JLang.guardarIdioma(idiomaId);
     });
+}
 
-});
 
 
 
