d916139fe64ce63407d298a5975a68cb1b694b0d
StevenT96 <steveng.carrion@gmail.com>
2018-03-19 16:29:25 +0100

    Añadida página home del músico

---
+++ html/pagina_musico.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <style>
+        #pm_divMusicoMain {
+
+           position: relative;
+           display: inline-block;
+           width: 100%;
+           height: 100%;
+           background-color: orange;
+        }
+    </style>
+    <body>
+        <div class="_childContainer divPadding10">
+            <div id="pm_divMusicoMain">
+
+            </div>
+        </div>
+    </body>
+</html>
