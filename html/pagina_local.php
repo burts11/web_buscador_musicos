02e7484a2a579f84aae1c6fcdb962beb83b5f6c5
StevenT96 <steveng.carrion@gmail.com>
2018-03-20 02:30:03 +0100

    Añadida página home de local

---
+++ html/pagina_local.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="../css/musico.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <style>
+        #divMusicosFan .musicoBody {
+
+            height: 75%;
+        }
+        
+        #divMusicosFan .musicoFooter {
+
+            height: 35%;
+        }
+        #divMusicosFan .musicoVoteContainer{
+
+            position: relative;
+            /*background-color: orange;*/
+            width: 1.2em;
+            height: 1.2em;
+            float: right;
+            right: 1.2em;
+        }
+
+        .musicoVoteBtn{
+
+            position: relative;
+            /*background-color: red;*/
+            width:100%;
+            height: 100%;
+        }
+
+        #divMusicos {
+
+            display: inline-block !important;
+            overflow: hidden;
+        }
+    </style>
+    <body>
+        <div class="_childContainer divPadding10">
+       
+        </div>
+    </body>
+</html>
