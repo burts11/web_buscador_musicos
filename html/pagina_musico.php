bd177bae3e969fd445b29d951ea03b8a542373e8
StevenT96 <steveng.carrion@gmail.com>
2018-03-22 00:42:22 +0100

    Empezando a definir el contenido de la página home de un músico

--- html/pagina_musico.php
+++ html/pagina_musico.php
@@ -14,9 +14,9 @@
         }
     </style>
     <body>
-        <div class="_childContainer divPadding10">
+        <div class="_childContainer">
             <div id="pm_divMusicoMain">
-
+                <h1>Aquí va el contenido de la página home del músico</h1>
             </div>
         </div>
     </body>
