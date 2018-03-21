839e47dd2e74d31fb9f18b95ae8d866794b80a92
StevenT96 <steveng.carrion@gmail.com>
2018-03-22 00:42:37 +0100

    Empezando a definir el contenido de la página home de un local

--- html/pagina_local.php
+++ html/pagina_local.php
@@ -5,42 +5,19 @@
         <link href="../css/musico.css" rel="stylesheet" type="text/css"/>
     </head>
     <style>
-        #divMusicosFan .musicoBody {
-
-            height: 75%;
-        }
-        
-        #divMusicosFan .musicoFooter {
-
-            height: 35%;
-        }
-        #divMusicosFan .musicoVoteContainer{
+        #pl_divLocalMain{
 
             position: relative;
-            /*background-color: orange;*/
-            width: 1.2em;
-            height: 1.2em;
-            float: right;
-            right: 1.2em;
-        }
-
-        .musicoVoteBtn{
-
-            position: relative;
-            /*background-color: red;*/
-            width:100%;
-            height: 100%;
-        }
-
-        #divMusicos {
-
-            display: inline-block !important;
-            overflow: hidden;
+            display: inline-block;
+            width: 100%;
+            height : 100%;
         }
     </style>
     <body>
         <div class="_childContainer divPadding10">
-       
+            <div id="pl_divLocalMain">
+                <h1>Aquí va el contenido de la página home del local</h1>
+            </div>
         </div>
     </body>
 </html>
