09d5c8e4ce44fcb1fa580a4c44d61a42adee7c8e
JaviB <JaviB@DESKTOP-OMPU3BH>
2018-05-18 16:58:03 +0200

    Lista de todos los conciertos aceptados y se borran en pendientes

--- html/pagina_local.php
+++ html/pagina_local.php
@@ -9,7 +9,7 @@
             display: inline-block;
             margin-right: 150px;
         }
-        #insc {
+        #aceptados {
             color:white;
         }
     </style>
@@ -34,7 +34,9 @@
             <div id="conciertos">
             </div>
 
-            <div id="insc"></div>
+            <div id="aceptados">
+                <h2>CONCIERTOS ACEPTADOS</h2>
+            </div>
         </div>
         <script>
 
@@ -43,7 +45,7 @@
             var idlocal = Usuario.id;
             var query = `select concierto.idconcierto,usuario.nombre as Local,concierto.fecha,concierto.hora,genero.nombre as genero,concierto.estado,concierto.valoreconomico
 from concierto join genero on concierto.genero = genero.idgenero
-join usuario on usuario.idusuario = concierto.idlocal where concierto.idlocal = ${idlocal}`;
+join usuario on usuario.idusuario = concierto.idlocal where concierto.idlocal = ${idlocal} and concierto.estado = 0`;
             var params = {
                 action: "RawQueryRet",
                 query: query};
@@ -51,82 +53,97 @@ join usuario on usuario.idusuario = concierto.idlocal where concierto.idlocal = 
                 $("#conciertos").empty();
 
                 $.each(result.data, function (i, item) {
-                    
+
                     var query = `select usuario.nombre from inscripcion join usuario on inscripcion.idmusico = usuario.idusuario where inscripcion.estado = 2 and idconcierto = ${item.idconcierto}`;
-                     var params = {
-                action: "RawQueryRet",
-                query: query};
-            callAjaxBBDD(params, function (result) {
-                
-                               console.log("inscripciomnnnnnn");
+                    var params = {
+                        action: "RawQueryRet",
+                        query: query};
+                    callAjaxBBDD(params, function (result) {
 
-                console.log(result);
-                    var ConsultarUsuarios = $("<button type='button' class='form-control-btn'>Consultar Inscritos</button>");
+                        var ConsultarUsuarios = $("<button type='button' class='form-control-btn'>Consultar Inscritos</button>");
 
-                    var divpadre = $("<div></div>");
-                    $(divpadre).addClass("div_concierto");
-                    $(divpadre).append("<label  class='blockLabel'> Local: " + item.Local + "</label>");
-                    $(divpadre).append("<label  class='blockLabel'> Fecha: " + item.fecha + "</label>");
-                    $(divpadre).append("<label  class='blockLabel'> Hora: " + item.hora + "</label>");
-                    $(divpadre).append("<label  class='blockLabel'> Genero: " + item.genero + "</label>");
-                    $(divpadre).append("<label  class='blockLabel'> Valor Economico: " + item.valoreconomico + "</label>");
-                    if (item.estado === 0) {
+                        var divpadre = $("<div></div>");
+                        $(divpadre).addClass("div_concierto");
+                        $(divpadre).append("<label  class='blockLabel'> Local: " + item.Local + "</label>");
+                        $(divpadre).append("<label  class='blockLabel'> Fecha: " + item.fecha + "</label>");
+                        $(divpadre).append("<label  class='blockLabel'> Hora: " + item.hora + "</label>");
+                        $(divpadre).append("<label  class='blockLabel'> Genero: " + item.genero + "</label>");
+                        $(divpadre).append("<label  class='blockLabel'> Valor Economico: " + item.valoreconomico + "</label>");
                         $(divpadre).append("<label  class='blockLabel'>Pendiente de selección</label>");
-                    } else if (item.estado === 1){
-                        $(divpadre).append("<label  class='blockLabel'>Musico aceptado: "+result.data[0].nombre+" </label>");
 
-                    }
 
-                    var botonBaja = $("<button type='button' class='form-control-btn'>DAR DE BAJA</button>");
-                  
+                        var botonBaja = $("<button type='button' class='form-control-btn'>DAR DE BAJA</button>");
 
-                    $(botonBaja).click(function () {
-                        var id = item.idconcierto;
-                        var query = `delete from concierto where idconcierto = ${id}`;
 
-                        var params = {
-                            action: "RawQueryRet",
-                            query: query};
-                        callAjaxBBDD(params, function (result) {
-                            var query = `delete from inscripcion where idconcierto = ${id}`;
-                              var params = {
-                            action: "RawQueryRet",
-                            query: query};
-                         callAjaxBBDD(params, function (result) {
-                             VToast.mostrarMensaje("Concierto dado de baja");
-                                    
+                        $(botonBaja).click(function () {
+                            var id = item.idconcierto;
+                            var query = `delete from concierto where idconcierto = ${id}`;
+
+                            var params = {
+                                action: "RawQueryRet",
+                                query: query};
+                            callAjaxBBDD(params, function (result) {
+                                var query = `delete from inscripcion where idconcierto = ${id}`;
+                                var params = {
+                                    action: "RawQueryRet",
+                                    query: query};
+                                callAjaxBBDD(params, function (result) {
+                                    VToast.mostrarMensaje("Concierto dado de baja");
+
                                     e.json.vparams.close();
                                 });
-                        });
-                        
-                    });
-                    
-                     $(divpadre).append(botonBaja);
-                     $(divpadre).append(ConsultarUsuarios);
-                    $(divpadre).append("<br></br>");
-                    $("#conciertos").append(divpadre);
+                            });
 
-                     $(ConsultarUsuarios).click(function () {
+                        });
 
-                        VModal.show("Consultar_Inscritos", "", {modalEffect: "vModalFadeIn-show", VModalId: "Consultar_Inscritos", CustomPadding: "true",
-                            padding: "0px", modalTop: "40%"}, {
-                            onDialogShow: function (ev) {
-                                ev["vparams"]["idconcierto"] = item.idconcierto;
-                                callJqueryWindowEvent(VInfo.CONSULTAR, ev);
-                            },
-                            onDialogClose: function (ev) {
-                            }
+                        $(divpadre).append(botonBaja);
+                        $(divpadre).append(ConsultarUsuarios);
+                        $(divpadre).append("<br></br>");
+                        $("#conciertos").append(divpadre);
+
+                        $(ConsultarUsuarios).click(function () {
+
+                            VModal.show("Consultar_Inscritos", "", {modalEffect: "vModalFadeIn-show", VModalId: "Consultar_Inscritos", CustomPadding: "true",
+                                padding: "0px", modalTop: "40%"}, {
+                                onDialogShow: function (ev) {
+                                    ev["vparams"]["idconcierto"] = item.idconcierto;
+                                    callJqueryWindowEvent(VInfo.CONSULTAR, ev);
+                                },
+                                onDialogClose: function (ev) {
+                                }
+                            });
                         });
                     });
-                });
-                   
+
                 });
 
 
             });
         </script>
+        <script>
+            var query = `select usuario.nombre,concierto.fecha,concierto.hora,genero.nombre as genero,comunidades.munucipio,comunidades.provincia from concierto 
+                join usuario on concierto.idmusico = usuario.idusuario join genero on concierto.genero = genero.idgenero
+join comunidades on concierto.ciudad = comunidades.idciudad where concierto.estado = 1`;
+            var params = {
+                action: "RawQueryRet",
+                query: query};
+            callAjaxBBDD(params, function (result) {
+                console.log(result);
+                $.each(result.data, function (i, item) {
+                    var divpadre = $("<div></div>");
+                    $(divpadre).addClass("div_aceptados");
+                    $(divpadre).append("<label  class='blockLabel'> Musico/Grupo: " + item.nombre + "</label>");
+                    $(divpadre).append("<label  class='blockLabel'> Fecha: " + item.fecha + "</label>");
+                    $(divpadre).append("<label  class='blockLabel'> Hora: " + item.hora + "</label>");
+                    $(divpadre).append("<label  class='blockLabel'> Genero: " + item.genero + "</label>");
+                    $(divpadre).append("<label  class='blockLabel'> Provincia: " + item.provincia + "</label>");
+                    $(divpadre).append("<label  class='blockLabel'> Municipio: " + item.munucipio + "</label>");
+                    $("#aceptados").append(divpadre);
+                });
+            });
+        </script>
 
 
-        
+
     </body>
 </html>
