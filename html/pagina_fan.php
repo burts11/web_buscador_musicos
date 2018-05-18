75e02dec875f3bb2ec5795572c31b538eb7c4c61
StevenT96 <steveng.carrion@gmail.com>
2018-05-18 19:50:08 +0200

    Implementada la votación de los músicos y conciertos

--- html/pagina_fan.php
+++ html/pagina_fan.php
@@ -5,16 +5,39 @@
         <link href="../css/musico.css" rel="stylesheet" type="text/css"/>
     </head>
     <style>
+        .conciertoContainer{
+
+            display: inline-block;
+            padding: 1em;
+            /*background-color: orange;*/   
+        }
+
+        #divConciertosFan .conciertoAlbumArtContainer{
+
+            position: relative;
+            height: 80%;
+        }
+
+        #divConciertosFan .conciertoBody {
+            height: 75%;
+        }
+
+        #divConciertosFan .conciertoFooter {
+
+            padding-top: 0.2em;
+            height: 35%;
+        }
 
         #divMusicosFan .musicoBody {
 
             height: 75%;
         }
-        
+
         #divMusicosFan .musicoFooter {
 
             height: 35%;
         }
+
         #divMusicosFan .musicoVoteContainer{
 
             position: relative;
@@ -25,12 +48,19 @@
             right: 1.2em;
         }
 
-        .musicoVoteBtn{
+        .musicoVoteBtn, .conciertoVoteBtn{
 
             position: relative;
             /*background-color: red;*/
             width:100%;
             height: 100%;
+        }
+
+        #divConciertosFan {
+
+            width: 100%;
+            display: inline-block !important;
+            overflow: hidden;
         }
 
         #divMusicos {
@@ -42,6 +72,9 @@
     <body>
         <div class="_childContainer divPadding10">
             <!--<h1 class="tituloH1 tituloH1-Grey">Página FAN</h1>-->
+            <div id="divConciertosFan">
+
+            </div>
             <div id="divMusicosFan">
 
             </div>
